17c93eb5870fc3d3eea48108b92b2e79dd979b10
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2026-09-11 22:00:43 +0200

    mix new ecto_spectral

---
+++ lib/ecto_spectral.ex
@@ -0,0 +1,18 @@
+defmodule EctoSpectral do
+  @moduledoc """
+  Documentation for `EctoSpectral`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoSpectral.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_spectral_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoSpectralTest do
+  use ExUnit.Case
+  doctest EctoSpectral
+
+  test "greets the world" do
+    assert EctoSpectral.hello() == :world
+  end
+end
